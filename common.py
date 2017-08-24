d95922b63c928afcf364c2ddf27cacc80a542fb7
sada-narayanappa <sada.narayanappa@gmail.com>
2017-08-24 01:34:59 -0600

    Commiting by Sada

---
+++ common.py
@@ -0,0 +1,25 @@
+import os
+import os.path
+import sys
+import re;
+import json;
+import os
+import numpy as np
+from IPython.display import HTML
+import pandas as pd
+from datetime import timedelta;
+import datetime;
+from random import randint
+from collections import defaultdict
+from pylab import rcParams
+
+import matplotlib.pyplot as plt
+import pandas as pd
+import glob;
+import re;
+import platform
+import matplotlib
+
+import dateutil;
+import json;
+import urllib.request;
