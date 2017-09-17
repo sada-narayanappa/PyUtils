c4cba6182abd793ec225ce3fea9d3c25e890d2ac
NARAYANAPPA, Sadananda (US) <sadananda.narayanappa@lmco.com>
2017-09-17 09:37:20 -0600

    Commiting by Sada

---
+++ Jupytils/__init__.py
@@ -0,0 +1,26 @@
+#
+#
+Jupytils_debug=0;
+
+def log(*args, debug=False, end=' ', **kwargs):
+    if (not debug or 'debug' in kwargs and not kwargs(debug) ):
+        return;
+
+    for a in args:
+        print(a, end=end)
+    for k,v in kwargs.items():
+        print ("%s = %s" % (k, v))
+
+log("This will load or reload Jupytils; This message will be removed in Future Versions", debug=Jupytils_debug)
+
+#
+import sys
+dels=[]
+for m in sys.modules:
+    if (m.startswith('Jupytils')):
+        dels.append(m)
+
+#print("Modules loaded: ", dels)
+
+from Jupytils.jcommon import *
+LoadJupytils()
