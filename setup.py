ee77f8504b067d646ccf3b327b53d4fb6c72add1
NARAYANAPPA, Sadananda (US) <sadananda.narayanappa@lmco.com>
2017-09-16 21:23:29 -0600

    Commiting by Sada

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='Jupytils',
-      version='0.236',
+      version='0.238',
       description='Productive Simple Intuitive Jupyter Python Utilities',
       url='https://github.com/sada-narayanappa/PyUtils.git',
       author='Code Red',
